4b0242cf654f8c2e4899bbcfb9191dbfd5d53fe0
tinybeer <543595604@qq.com>
2024-05-05 10:14:54 +0800

    57.插入区间

---
+++ 57.插入区间/main.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+给你一个 无重叠的 ，按照区间起始端点排序的区间列表 intervals，其中 intervals[i] = [starti, endi] 表示第 i 个区间的开始和结束，并且 intervals 按照 starti 升序排列。同样给定一个区间 newInterval = [start, end] 表示另一个区间的开始和结束。
+
+在 intervals 中插入区间 newInterval，使得 intervals 依然按照 starti 升序排列，且区间之间不重叠（如果有必要的话，可以合并区间）。
+
+返回插入之后的 intervals。
+
+注意 你不需要原地修改 intervals。你可以创建一个新数组然后返回它。
+*/
+
+func insert(intervals [][]int, newInterval []int) [][]int {
+	left, right := newInterval[0], newInterval[1]
+	merged := false
+	var ans [][]int
+	for _, interval := range intervals {
+		if interval[0] > right {
+			// 在插入区间的右侧且无交集
+			if !merged {
+				ans = append(ans, []int{left, right})
+				merged = true
+			}
+			ans = append(ans, interval)
+		} else if interval[1] < left {
+			// 在插入区间的左侧且无交集
+			ans = append(ans, interval)
+		} else {
+			// 与插入区间有交集，计算它们的并集
+			left = min(left, interval[0])
+			right = max(right, interval[1])
+		}
+	}
+	if !merged {
+		ans = append(ans, []int{left, right})
+	}
+	return ans
+
+}
