c4986431589485da6f231565889f5b4c307e7602
tinybeer <543595604@qq.com>
2024-05-05 09:19:19 +0800

    56.合并区间

---
+++ 56.合并区间/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		intervals [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "intervals = [[1,3],[2,6],[8,10],[15,18]]",
+			args: args{
+				intervals: [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+			},
+			want: [][]int{{1, 6}, {8, 10}, {15, 18}},
+		},
+		{
+			name: "intervals = [[1,4],[4,5]]",
+			args: args{
+				intervals: [][]int{{1, 4}, {4, 5}},
+			},
+			want: [][]int{{1, 5}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := merge(tt.args.intervals); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("merge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
