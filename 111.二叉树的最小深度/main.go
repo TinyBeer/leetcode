6dfb90adf0c4a0f34a023ea3f0e320cc854a5be2
tinybeer <543595604@qq.com>
2024-05-16 15:51:56 +0800

    111.二叉树的最小深度

---
+++ 111.二叉树的最小深度/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "runtime/debug"
+
+/*
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+*/
+
+func init() {
+	// 禁用gc
+	debug.SetGCPercent(-1)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+func min(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
---
+++ 111.二叉树的最小深度/main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import "testing"
+
+func Test_minDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "root = [3,9,20,null,null,15,7]",
+			args: args{
+				root: &TreeNode{
+					Val:  3,
+					Left: &TreeNode{Val: 9},
+					Right: &TreeNode{
+						Val:   20,
+						Left:  &TreeNode{Val: 15},
+						Right: &TreeNode{Val: 7},
+					},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "root = [2,null,3,null,4,null,5,null,6]",
+			args: args{
+				root: &TreeNode{
+					Val: 2,
+					Right: &TreeNode{
+						Val: 3,
+						Right: &TreeNode{
+							Val: 4,
+							Right: &TreeNode{
+								Val:   5,
+								Right: &TreeNode{Val: 6},
+							},
+						},
+					},
+				},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minDepth(tt.args.root); got != tt.want {
+				t.Errorf("minDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
