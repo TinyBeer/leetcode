c3696affeb79b39671d610b51bb3fc1c741df066
tinybeer <543595604@qq.com>
2024-05-17 22:35:50 +0800

    118.杨辉三角

---
+++ 118.杨辉三角/main.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+*/
+
+func generate(numRows int) [][]int {
+	if numRows == 0 {
+		return nil
+	}
+	res := make([][]int, numRows)
+	for i := range res {
+		res[i] = make([]int, i+1)
+		res[i][0] = 1
+		res[i][i] = 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
+		}
+	}
+	return res
+}
