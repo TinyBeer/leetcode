789d4fd41db385a574adce5a9c384542d01d88aa
tinybeer <543595604@qq.com>
2024-05-19 10:35:17 +0800

    125.验证回文串

---
+++ 125.验证回文串/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "strings"
+
+/*
+如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。
+则可以认为该短语是一个 回文串 。
+
+字母和数字都属于字母数字字符。
+
+给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+*/
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	i, j := 0, len(s)-1
+	for i < j {
+		for i < j && !isAphaNum(s[i]) {
+			i++
+		}
+		for i < j && !isAphaNum(s[j]) {
+			j--
+		}
+		if i >= j {
+			return true
+		}
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func isAphaNum(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
+}
