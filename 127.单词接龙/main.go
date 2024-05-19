7180fa7ecd41ea3628df872b50b0a612ac893022
tinybeer <543595604@qq.com>
2024-05-19 20:23:57 +0800

    127.单词接龙

---
+++ 127.单词接龙/main.go
@@ -0,0 +1,70 @@
+package main
+
+import "math"
+
+/*
+字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列 beginWord -> s1 -> s2 -> ... -> sk：
+
+每一对相邻的单词只差一个字母。
+ 对于 1 <= i <= k 时，每个 si 都在 wordList 中。注意， beginWord 不需要在 wordList 中。
+sk == endWord
+给你两个单词 beginWord 和 endWord 和一个字典 wordList ，返回 从 beginWord 到 endWord 的 最短转换序列 中的
+单词数目 。如果不存在这样的转换序列，返回 0 。
+*/
+
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	wordId := map[string]int{}
+	graph := [][]int{}
+	addWord := func(word string) int {
+		id, has := wordId[word]
+		if !has {
+			id = len(wordId)
+			wordId[word] = id
+			graph = append(graph, []int{})
+		}
+		return id
+	}
+	addEdge := func(word string) int {
+		id1 := addWord(word)
+		s := []byte(word)
+		for i, b := range s {
+			s[i] = '*'
+			id2 := addWord(string(s))
+			graph[id1] = append(graph[id1], id2)
+			graph[id2] = append(graph[id2], id1)
+			s[i] = b
+		}
+		return id1
+	}
+
+	for _, word := range wordList {
+		addEdge(word)
+	}
+	beginId := addEdge(beginWord)
+	endId, has := wordId[endWord]
+	if !has {
+		return 0
+	}
+
+	const inf int = math.MaxInt64
+	dist := make([]int, len(wordId))
+	for i := range dist {
+		dist[i] = inf
+	}
+	dist[beginId] = 0
+	queue := []int{beginId}
+	for len(queue) > 0 {
+		v := queue[0]
+		queue = queue[1:]
+		if v == endId {
+			return dist[endId]/2 + 1
+		}
+		for _, w := range graph[v] {
+			if dist[w] == inf {
+				dist[w] = dist[v] + 1
+				queue = append(queue, w)
+			}
+		}
+	}
+	return 0
+}
