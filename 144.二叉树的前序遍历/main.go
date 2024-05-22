bce4cc35dd3be45381452508d0f3a9400227a5bb
tinybeer <543595604@qq.com>
2024-05-22 12:50:18 +0800

    144.二叉树的前序遍历

---
+++ 144.二叉树的前序遍历/main.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var ans []int
+	var df func(*TreeNode)
+	df = func(tn *TreeNode) {
+		if tn == nil {
+			return
+		}
+		ans = append(ans, tn.Val)
+		df(tn.Left)
+		df(tn.Right)
+	}
+}
