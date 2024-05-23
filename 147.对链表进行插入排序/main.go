b3e7fadd72210e2abfde748a548ffd806501905b
tinybeer <543595604@qq.com>
2024-05-23 15:37:34 +0800

    147。对链表进行插入排序

---
+++ 147.对链表进行插入排序/main.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+给定单个链表的头 head ，使用 插入排序 对链表进行排序，并返回 排序后链表的头 。
+
+插入排序 算法的步骤:
+
+插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+重复直到所有输入数据插入完为止。
+下面是插入排序算法的一个图形示例。部分排序的列表(黑色)最初只包含列表中的第一个元素。每次迭代时，从输入数据中删除一个元素(红色)，并就地插入已排序的列表中。
+
+对链表进行插入排序。
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func insertionSortList(head *ListNode) *ListNode {
+	tmp := &ListNode{Next: head}
+	for head.Next != nil {
+		if head.Val < head.Next.Val {
+			head = head.Next
+			continue
+		}
+		ptr := tmp
+		for ptr.Next.Val < head.Next.Val {
+			ptr = ptr.Next
+		}
+		helper := head.Next
+		head.Next = helper.Next
+		helper.Next = ptr.Next
+		ptr.Next = helper
+	}
+	return tmp.Next
+}
