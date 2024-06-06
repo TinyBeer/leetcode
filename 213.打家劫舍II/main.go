e1ee3df21b68d37366f38d97a6c0f933b003006b
tinybeer <543595604@qq.com>
2024-06-06 12:02:17 +0800

    213.打家劫舍II a

--- 213.打家劫舍II/main.go
+++ 213.打家劫舍II/main.go
@@ -6,32 +6,58 @@ package main
 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
 */
 
+func _rob(nums []int) int {
+	first, second := nums[0], max(nums[0], nums[1])
+	for _, v := range nums[2:] {
+		first, second = second, max(first+v, second)
+	}
+	return second
+}
+
 func rob(nums []int) int {
-	if len(nums) == 1 {
+	n := len(nums)
+	if n == 1 {
 		return nums[0]
 	}
-	if len(nums) == 2 {
+	if n == 2 {
 		return max(nums[0], nums[1])
 	}
-	maxRob := func(ns []int) int {
-		if len(ns) == 0 {
-			return 0
-		}
-		if len(ns) == 1 {
-			return ns[0]
-		}
-		dp, dp2 := make([]int, len(ns)), make([]int, len(ns))
-		dp[0] = ns[0]
-		dp[1] = ns[1]
-		dp2[1] = ns[0]
-		for i := 2; i < len(ns); i++ {
-			dp[i] = max(dp[i-2], dp2[i-1]) + ns[i]
-			dp2[i] = max(dp[i-1], dp2[i-1])
-		}
-		return max(dp[len(ns)-1], dp2[len(ns)-1])
-	}
-	return max(maxRob(nums[1:]), nums[0]+maxRob(nums[2:len(nums)-1]))
+	return max(_rob(nums[:n-1]), _rob(nums[1:]))
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// func rob(nums []int) int {
+// 	if len(nums) == 1 {
+// 		return nums[0]
+// 	}
+// 	if len(nums) == 2 {
+// 		return max(nums[0], nums[1])
+// 	}
+// 	maxRob := func(ns []int) int {
+// 		if len(ns) == 0 {
+// 			return 0
+// 		}
+// 		if len(ns) == 1 {
+// 			return ns[0]
+// 		}
+// 		dp, dp2 := make([]int, len(ns)), make([]int, len(ns))
+// 		dp[0] = ns[0]
+// 		dp[1] = ns[1]
+// 		dp2[1] = ns[0]
+// 		for i := 2; i < len(ns); i++ {
+// 			dp[i] = max(dp[i-2], dp2[i-1]) + ns[i]
+// 			dp2[i] = max(dp[i-1], dp2[i-1])
+// 		}
+// 		return max(dp[len(ns)-1], dp2[len(ns)-1])
+// 	}
+// 	return max(maxRob(nums[1:]), nums[0]+maxRob(nums[2:len(nums)-1]))
+// }
 
 // func rob(nums []int) int {
 // 	if len(nums) == 1 {
