ce9a88c19023495c0b4414f3b9a856dad9758990
tinybeer <543595604@qq.com>
2024-06-06 12:19:25 +0800

    214.最短回文串

---
+++ 214.最短回文串/main.go
@@ -0,0 +1,66 @@
+package main
+
+/*
+给定一个字符串 s，你可以通过在字符串前面添加字符将其转换为
+回文串
+。找到并返回可以用这种方式转换的最短回文串。
+*/
+
+func shortestPalindrome(s string) string {
+	n := len(s)
+	fail := make([]int, n)
+	for i := 0; i < n; i++ {
+		fail[i] = -1
+	}
+	for i := 1; i < n; i++ {
+		j := fail[i-1]
+		for j != -1 && s[j+1] != s[i] {
+			j = fail[j]
+		}
+		if s[j+1] == s[i] {
+			fail[i] = j + 1
+		}
+	}
+	best := -1
+	for i := n - 1; i >= 0; i-- {
+		for best != -1 && s[best+1] != s[i] {
+			best = fail[best]
+		}
+		if s[best+1] == s[i] {
+			best++
+		}
+	}
+	add := ""
+	if best != n-1 {
+		add = s[best+1:]
+	}
+	b := []byte(add)
+	for i := 0; i < len(b)/2; i++ {
+		b[i], b[len(b)-1-i] = b[len(b)-1-i], b[i]
+	}
+	return string(b) + s
+}
+
+// func shortestPalindrome(s string) string {
+// 	n := len(s)
+// 	base, mod := 131, 1000000007
+// 	left, right, mul := 0, 0, 1
+// 	best := -1
+// 	for i := 0; i < n; i++ {
+// 		left = (left*base + int(s[i]-'0')) % mod
+// 		right = (right + mul*int(s[i]-'0')) % mod
+// 		if left == right {
+// 			best = i
+// 		}
+// 		mul = mul * base % mod
+// 	}
+// 	add := ""
+// 	if best != n-1 {
+// 		add = s[best+1:]
+// 	}
+// 	b := []byte(add)
+// 	for i := 0; i < len(b)/2; i++ {
+// 		b[i], b[len(b)-1-i] = b[len(b)-1-i], b[i]
+// 	}
+// 	return string(b) + s
+// }
