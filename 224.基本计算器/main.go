733dcf38dd531d8ac11c95cad5b3180446904b37
tinybeer <543595604@qq.com>
2024-06-11 14:49:48 +0800

    224.基本计算器

---
+++ 224.基本计算器/main.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+
+注意:不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。
+*/
+func calculate(s string) int {
+	ops := []int{1}
+	sign := 1
+	n := len(s)
+	ans := 0
+
+	for i := 0; i < n; {
+		switch s[i] {
+		case ' ':
+			i++
+		case '+':
+			sign = ops[len(ops)-1]
+			i++
+		case '-':
+			sign = -ops[len(ops)-1]
+			i++
+		case '(':
+			ops = append(ops, sign)
+			i++
+		case ')':
+			ops = ops[:len(ops)-1]
+			i++
+		default:
+			num := 0
+			for ; i < n && '0' <= s[i] && s[i] <= '9'; i++ {
+				num = num*10 + int(s[i]-'0')
+			}
+			ans += sign * num
+		}
+	}
+	return ans
+}
