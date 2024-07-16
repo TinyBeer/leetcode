8f72c906e596edcb83bcd965fb3dbed0158bfb73
tinybeer <543595604@qq.com>
2024-07-16 22:37:47 +0800

    327.区间和的个数

---
+++ 327.区间和的个数/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func Test_countRangeSum(t *testing.T) {
+	type args struct {
+		nums  []int
+		lower int
+		upper int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "nums = [-2,5,-1], lower = -2, upper = 2",
+			args: args{
+				nums:  []int{-2, 5, -1},
+				lower: -2,
+				upper: 2,
+			},
+			want: 3,
+		},
+		{
+			name: "nums = [0], lower = 0, upper = 0",
+			args: args{
+				nums:  []int{0},
+				lower: 0,
+				upper: 0,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countRangeSum(tt.args.nums, tt.args.lower, tt.args.upper); got != tt.want {
+				t.Errorf("countRangeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
