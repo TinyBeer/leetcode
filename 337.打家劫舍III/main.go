f297ff34ae2e2c46d0538f7f4a492966f2c81c43
tinybeer <543595604@qq.com>
2024-07-23 20:27:01 +0800

    337.打家劫舍

---
+++ 337.打家劫舍III/main.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+
+除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+
+给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+*/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rob(root *TreeNode) int {
+	val := dfs(root)
+	return max(val[0], val[1])
+}
+
+func dfs(node *TreeNode) []int {
+	if node == nil {
+		return []int{0, 0}
+	}
+	l, r := dfs(node.Left), dfs(node.Right)
+	selected := node.Val + l[1] + r[1]
+	notSelected := max(l[0], l[1]) + max(r[0], r[1])
+	return []int{selected, notSelected}
+}
