afa39ad4ee23c2e48ca60649f379080960ca08bf
tinybeer <543595604@qq.com>
2024-07-31 20:28:19 +0800

    354.俄罗斯套娃信封问题

---
+++ 354.俄罗斯套娃信封问题/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "sort"
+
+/*
+给你一个二维整数数组 envelopes ，其中 envelopes[i] = [wi, hi] ，表示第 i 个信封的宽度和高度。
+
+当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+
+请计算 最多能有多少个 信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+
+注意：不允许旋转信封。
+*/
+
+func maxEnvelopes(envelopes [][]int) int {
+	sort.Slice(envelopes, func(i, j int) bool {
+		return envelopes[i][0] < envelopes[j][0] || envelopes[i][0] == envelopes[j][0] && envelopes[i][1] > envelopes[j][1]
+	})
+	var f []int
+	for _, e := range envelopes {
+		if i := sort.SearchInts(f, e[1]); i < len(f) {
+			f[i] = e[1]
+		} else {
+			f = append(f, e[1])
+		}
+	}
+	return len(f)
+}
