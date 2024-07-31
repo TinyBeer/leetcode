793d32e45d410e5e6f207421e9ebc5726b24b151
tinybeer <543595604@qq.com>
2024-07-31 20:22:36 +0800

    350.两个数组的交集II

---
+++ 350.两个数组的交集II/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "sort"
+
+/*
+给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，
+应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+*/
+
+func intersect(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	length1, length2 := len(nums1), len(nums2)
+	index1, index2 := 0, 0
+
+	intersection := []int{}
+	for index1 < length1 && index2 < length2 {
+		if nums1[index1] < nums2[index2] {
+			index1++
+		} else if nums1[index1] > nums2[index2] {
+			index2++
+		} else {
+			intersection = append(intersection, nums1[index1])
+			index1++
+			index2++
+		}
+	}
+	return intersection
+}
