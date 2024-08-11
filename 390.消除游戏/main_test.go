04698157dc19b32fdcd2af2b6e3e1a2c6751d55f
tinybeer <543595604@qq.com>
2024-08-11 17:23:27 +0800

    390.消除游戏

---
+++ 390.消除游戏/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func Test_lastRemaining(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "n = 9",
+			args: args{
+				n: 9,
+			},
+			want: 6,
+		},
+		{
+			name: "n = 1",
+			args: args{
+				n: 1,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lastRemaining(tt.args.n); got != tt.want {
+				t.Errorf("lastRemaining() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
