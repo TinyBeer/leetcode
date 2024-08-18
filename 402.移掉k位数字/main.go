9bf4845e7777ed2796494c9efbda912047b449f7
tinybeer <543595604@qq.com>
2024-08-18 17:53:40 +0800

    402.移掉k位数字

---
+++ 402.移掉k位数字/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "strings"
+
+/*
+给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+*/
+
+func removeKdigits(num string, k int) string {
+	stack := []byte{}
+	for i := range num {
+		digit := num[i]
+		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, digit)
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		ans = "0"
+	}
+	return ans
+}
